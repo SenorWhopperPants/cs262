4c96af8f3a93dd3f1ad96974f86735273a5fae5f
JoshBussis <jmb237@students.calvin.edu>
2019-10-01 20:18:52 -0400

    adding lab05

---
+++ lab05/ionic-todo/src/app/edit-todo/edit-todo.page.ts
@@ -0,0 +1,30 @@
+import { Component, OnInit } from '@angular/core';
+import { Todo } from '../interfaces/todo';
+
+@Component({
+  selector: 'app-edit-todo',
+  templateUrl: './edit-todo.page.html',
+  styleUrls: ['./edit-todo.page.scss'],
+})
+export class EditTodoPage implements OnInit {
+
+  private todo: Todo;
+
+  constructor() { 
+    
+    this.todo = {
+      id: 0,
+      title: '',
+      description: ''
+    };
+  }
+
+  ngOnInit() {
+    // grab id from route
+  }
+
+  saveTodo() {
+
+  }
+
+}
---
+++ lab05/ionic-todo/src/app/services/todo.service.ts
@@ -0,0 +1,28 @@
+import { Injectable } from '@angular/core';
+import { Todo } from '../interfaces/todo';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class TodoService {
+
+  public todos: Todo[] = [];
+
+  constructor() { }
+
+  load() {
+
+  }
+
+  save() {
+
+  }
+
+  getTodo(id) {
+
+  }
+
+  addTodo(todo) {
+
+  }
+}
