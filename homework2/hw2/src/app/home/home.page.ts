8364a78ef93f8902563914a2090906eb24f1547c
JoshBussis <jmb237@students.calvin.edu>
2019-10-16 23:07:06 -0400

    adding homework2

---
+++ homework2/hw2/src/app/home/home.page.ts
@@ -0,0 +1,18 @@
+import { Component } from '@angular/core';
+import { Router } from '@angular/router';
+
+@Component({
+  selector: 'app-home',
+  templateUrl: 'home.page.html',
+  styleUrls: ['home.page.scss'],
+})
+export class HomePage {
+
+  constructor(private router: Router) {}
+
+  settingsClick() {
+    // set navigation to settings page here
+    this.router.navigateByUrl('settings');
+  }
+
+}
